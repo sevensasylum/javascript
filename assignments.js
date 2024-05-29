b3e98694bcc526cb72cc1a1c4a492d596d6c7f15
sevensasylum <sevensasylum@gmail.com>
2024-05-29 04:48:09 -0400

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,9 @@
+//Values & Variables
+
+/*let country = 'United States';
+let continent = 'North America';
+let population = 333;
+
+console.log(country);
+console.log(continent);
+console.log(population);*/
---
+++ script.js
@@ -0,0 +1,84 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Seven');
+console.log(29);
+
+let firstName = "Seven";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let seven_jonas = 'SJ';
+
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "Seven with the Mac11");
+
+//Reassigning the type
+javascriptIsFun = "Changed";
+console.log(typeof javascriptIsFun);
+
+//Undefined values
+let year;
+let age;
+
+console.log(year)// Undefined
+console.log(age)//Undefined
+console.log(typeof year);
+console.log(typeof age);
+
+//Assign Value
+
+year = 1994;
+age = 29;
+
+console.log(typeof year);
+console.log(typeof age);
+
+
+// Let, Const, & Var
+
+let age = 29;
+age = 30;
+
+const birthYear = 1994; 
+
+var age = 29;
+age = 30;
+
+console.log(age);*/
+
+//Basic Operators (Arithmatic) *, +, /, -
+
+const current = 2024;
+const ageSeven = current - 1994;
+const ageMary = current - 1974;
+console.log(ageSeven, ageMary);
+
+console.log(ageSeven * 3, ageMary/ 7, ageSeven + ageMary, 2 ** 3);
+//2 ** 3 is 2 to the power of 3 which = to 8
+
+//The "+" operator can be used in math or to concatenate strings together
+//Concatenate using the + operator.
+const firstName = 'Seven';
+const lastName = 'Sosa';
+
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;// 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // adding by 1
+x--; // subtracting by 1
+x--;
+console.log(x);
+
+// Comparison operations >, >=, <=, <
+console.log(ageSeven < ageMary);
+console.log(ageMary >= 18);
