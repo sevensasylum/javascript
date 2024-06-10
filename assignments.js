57319787970f6e0cd97c124bdadcc7d83df4eb5f
Seven <sevensasylum@gmail.com>
2024-06-09 22:41:20 -0400

    changes

--- assignments.js
+++ assignments.js
@@ -1,30 +1,130 @@
 ///////Values & Variables//////
 
-const country = 'United States';
-const continent = 'North America';
-let  population = 333;
+// const country = 'United States'; //correct
+// const continent = 'North America'; //correct
+// let  population = 333; //correct
 
-console.log(country);
-console.log(continent);
-console.log(population);
+// console.log(country); //correct
+// console.log(continent); //correct
+// console.log(population); //correct
 
 
 /////Data Types///////
 
-let isIsland = false;
-let language;
-isIsland = true;
-console.log(typeof isIsland);
-console.log(typeof population);
-console.log(typeof country);
-console.log(typeof language);
+// let isIsland = false; //correct
+// let language; //correct
+// isIsland = true; //correct
+// console.log(typeof isIsland); //correct
+// console.log(typeof population); //correct
+// console.log(typeof country); //correct
+// console.log(typeof language); //correct
 
 ///////let, const, and var//////
 
-language = 'English';
+//language = 'English'; //correct
 
 ///////Basic Operators//////
 
 //1.
-console.log(population/2);
+//console.log(population/2); //correct
+
+//2.
+//population++; //correct
+//console.log(population); //correct
+
+//3.
+//console.log(country > 6); //correct
+
+//4.
+//console.log(country <= 33); //correct
+
+//5.
+// const description = country + ' is in ' + continent + ', and its ' + 
+// population + ' million people speak ' + language + '.'; //correct
+
+// console.log(description); //correct
+
+//Strings and Template Literals
+//recreate description into template literal.
+
+//const description1 = `${country} is in ${continent}, and its ${population} million people speak ${language}`; 
+//correct
+
+//console.log(description1); //correct
+
+//Taking if/else statements
+
+// const country = 'United States'; //correct
+// const continent = 'North America'; //correct
+// let  population = 15; //correct
+
+// if(population > 33){
+//     console.log(`${country}'s population is above average`);
+// } else {
+//     console.log(`${country} population is ${(33 - population)/2} million below average.`);
+
+// }
+
+//Type conversion and Type Coercion
+//Predict the result of these 5 operations without executing them
+
+// console.log('9' - '5'); //->  4
+// console.log('19' - '13' + '17'); //-> '617'
+// console.log('19' - '13' + 17); //-> 23
+// console.log('123' < 57); //-> false
+// console.log(5 + 6 + '4' + 9 - 4 - 2); //-> 1143
+
+//Equality Operators: == Vs. ===
+
+//1.
+// const numNeighbours = Number (prompt('How many neighbour countries does your country have?'));
+// //correct!
+
+// //2.
+// if(numNeighbours === 1){
+//     console.log('Only  1 border!'); //correct!
+// } else if (numNeighbours > 1) {
+//     console.log('More than 1 border.'); //correct!
+// } else {
+//     console.log('No Borders!'); //correct!
+// }
+
+
+//Logical Operators
+// const country = 'United States'; 
+// const continent = 'North America'; 
+// let  population = 33; 
+// let isIsland = false; 
+// let language;
+// language = 'English';
+
+// if(language === 'English' && population < 50 && !isIsland){
+//     console.log(`You should live in the ${country}!`);
+// } else {
+//     console.log(`${country} does not meet your criteria!`);
+// }
+
+//The Switch Statement
+// const language = 'mandarin';
+
+// switch(language) {
+//     case 'chinese':
+//     case 'mandarin':
+//         console.log('Most number of native speakers!');
+//         break;
+//     case 'spanish':
+//         console.log('2nd place in number of native speakers!');
+//         break;
+//     case 'english':
+//         console.log('3rd place!');
+//         break;
+//     case 'hindi':
+//         console.log('Number 4!');
+//         break;
+//     case 'arabic':
+//         console.log('5th most spoken language!');
+//         break;
+//     default:
+//         console.log('Great language too :D.');
+// }
 
--- script.js
+++ script.js
@@ -156,7 +156,7 @@ lines`);
 
 // Taking decisions and If Else Statements
 
-const age = 15;
+const age = 19;
 const isOldEnough = age >= 18;
 
 if(isOldEnough) {
@@ -166,7 +166,19 @@ console.log('Sarah can start driving license 🚗');
     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
 }
 
-const birthYear = 2012;
+another way: writing the condition directly in
+
+const age = 19;
+
+if(age >= 18){
+    console.log('Sarah can start driving license 🚗');
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+
+const birthYear = 1999;
 let century;
 
 if(birthYear <= 2000){
@@ -200,16 +212,16 @@ console.log(markHigherBMI);
 //create if else and template literal to say "Mark's BMI (28.3) is higher than John's (23.9)"
 
 if(BMIMark > BMIJohn){
-    console.log("Mark's BMI is higher than John's!")
+    console.log("Mark's BMI is higher than John's!") // supposed to add template here for challenge but see comments below***
 }else{
     console.log("John's BMI is higher than Mark's!")
 }
 
 //Template Literal
-console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`); 
+console.log(`Mark's BMI ${BMIMark} is higher than John's ${BMIJohn}!`); 
 //and vice versa, Im supposed to change it in the if else statement but for me to understand my comments I will leave it as is!
 
-//type conversion from one type to another using the "Number" function to convert
+//type conversion from one type to another using the "Number" function to convert and String and Boolean
 const inputYear = '1991';
 console.log(Number(inputYear), inputYear);
 console.log(Number(inputYear) + 18);
@@ -224,8 +236,8 @@ console.log(String(23));
 console.log('I am ' + 23 + ' years old');
 console.log('I am ' + '23' + ' years old');//This is the same thing as the one above because it converts it to a string because of type coercion.
 
-console.log('23' - '10' - 3);
-console.log('23' * '2');
+console.log('23' - '10' - 3); converted to numbers
+console.log('23' * '2'); converted to numbers
 
 let n = '1' + 1; // this concatenates to 11 and then subtracts 1 and results 10. It didnt stay a string because subtraction is not a concatenating string converter.
 n = n - 1; 
@@ -257,13 +269,12 @@ if(height){
 
 //Equality Operators == vs ===
 
-const age = '18';
+const age = 18;
 if(age === 18) console.log('You just became an adult :D (strict)');
 
-if(age == 18) console.log('You just became an adult :D (loose)');
+if(age == 18) console.log('You just became an adult :D (loosey)');
 
-const favorite = Number (prompt("What's your favorite number?")
-);
+const favorite = Number (prompt("What's your favorite number?"));
 console.log(favorite);
 console.log(typeof favorite);
 
@@ -277,8 +288,117 @@ if(favorite == 33) {
     console.log('Number is not 33 nor 7 nor 9')
 }
 
-if(favorite !== 33) console.log('Why not 33?');*/
+if(favorite !== 33) console.log('Why not 33?');
 
 //Boolean Logic
 
+const hasDriverLicense = true; //A
+const hasGoodVision = true; //B
+
+console.log(hasDriverLicense && hasGoodVision);//AND
+console.log(hasDriverLicense || hasGoodVision);//OR
+console.log(!hasDriverLicense);//NOT
+
+const shouldDrive = hasDriverLicense && hasGoodVision;//normally we put the condition in the parenthesis!
+
+// if(hasDriverLicense && hasGoodVision){
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+const isTired = false;//C
+console.log(hasDriverLicense && hasGoodVision || isTired);
+
+if(hasDriverLicense && hasGoodVision && !isTired){
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
+
+const day = 'tues';
+
+switch(day) {
+    case 'monday':
+        console.log('Plan course structure');
+        console.log('Go to coding meetup');
+        break;
+    case 'tuesday':
+        console.log('Prepare theory videos');
+        break;
+    case 'wednesday':
+    case 'thursday':
+        console.log('Write code examples');
+        break;
+    case 'friday':
+        console.log('Record videos');
+        break;
+    case 'saturday':
+    case 'sunday':
+        console.log('Enjoy the weekend :P');
+        break;
+    default:
+            console.log('Not a valid day!');
+}
+
+if(day === 'monday'){
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+} else if( day === 'tuesday'){
+    console.log('Prepare theory videos');
+} else if (day === 'wednesday' || day === 'thursday'){
+    console.log('Write code examples');
+} else if( day === 'friday'){
+    console.log('Record videos');
+} else if ( day === 'saturday' || day === 'sunday'){
+    console.log('Enjoy the weekend :P');
+} else {
+    console.log('Not a valid day!');
+}
+
+
+//Conditional (Ternary) Operator
+
+const age = 15;
+// age >= 18 ? console.log('I like to drink wine') : 
+// console.log('I like to drink water');
+
+const drink = age >= 18 ? 'wine' : 'water';
+console.log(drink);
+
+//better not to do it like this. the conditional operator would be better.
+let drink2;
+if(age >= 18){
+    drink2 = 'wine'
+}else{
+    drink2 = 'water'
+}
+console.log(drink2);
+
+//now we'll incorporate the teranary opertator into a template literal
+
+console.log(`I like to drink ${age >= 18 ? 'wine' : 'water'}`);*/
+
+/*CHALLENGE #4
+Steven needs a very simple tip calculator for whenever he goes to eat in a restaurant. In his country, it's usual to tip 15% if the bill value is between 50 and 300. If the value is different, the tip is 20%.
+
+Your tasks:
+
+Calculate the tip, depending on the bill value. Create a variable called tip for this. It's not allowed to use an if...else statement (if it's easier for you, you can start with an if...else statement, and then try to convert it to a ternary operator).
+
+Print a string to the console containing the bill value, the tip, and the final value (bill + tip).
+
+Example: The bill was 275, the tip was 41.25, and the total value 316.25.
+
+Note: Use the values of the bill and tip variables to construct this string. Don't hard-code them 🙂
+
+TEST DATA: Test with different bill values: 275, 40, and 430
+
+HINT: To calculate 20% of a value, simply multiply it by 20/100 = 0.2
+
+HINT 2: Value X is between 50 and 300, if it's >= 50 && <= 300 😉*/
+
+// const bill = 275;
+// const tip = bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+// console.log(`The bill was ${bill}, the tip was ${tip}, and the total value ${bill + tip}`);
 
